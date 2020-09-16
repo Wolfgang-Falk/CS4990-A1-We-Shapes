b301d1aabd77554d3991596db265865b10be440c
Sean <SeanSrockOHS@gmail.com>
2020-09-16 00:59:30 -0400

    Add PlayerController.cs, start adding some stuff to the testing scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // We want to use this to rotate the camera later.
+    public Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Attempt to get the camera's transform if we don't already have it.
+        if (cameraTransform == null)
+        {
+            Debug.LogWarning("Camera was not assigned, attempting to find it.");
+
+            var _tempObject = GameObject.Find("Main Camera");
+
+            if (_tempObject != null)
+            {
+                cameraTransform = _tempObject.transform;
+                Debug.Log("Camera found and assigned.");
+            }
+            else
+            {
+                Debug.LogError("Unable to get the Camera!");
+            }            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // TODO: Player Movement
+
+        // TODO: Shape Transformation
+
+        // TODO: Collectibles
+
+        // TODO: Camera Rotation based on collision with triggers.
+
+    }
+}
