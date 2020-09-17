826f8ec1dd1ae9eecd71188b699b6e9e4847277f
Sean <SeanSrockOHS@gmail.com>
2020-09-17 04:24:20 -0400

    Implement Main Menu buttons, move platform movement

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame(string sceneName)
+    {
+        Debug.Log("Attempting to go to scene: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowControls()
+    {
+        // TODO: Implement.
+        // if not shown, disable menu stuff, show controls, vice versa
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Ending the game.");
+        Application.Quit();
+    }
+
+}
