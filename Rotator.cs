7cc1b973154cd05a8606e26516835712f0e7f71d
Wade Falk <71107065+Wolfgang-Falk@users.noreply.github.com>
2020-09-24 22:53:29 -0500

    M5 Patch - Wade

---
+++ Rotator.cs
@@ -0,0 +1,19 @@
+﻿/* ****************************************************
+ * File:		Rotator
+ * Purpose:		To constantly rotate Pick Up
+ * Programmer:	Wade Falk
+ * Date:		September 24th, 2020
+ *************************************************** */
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour 
+{
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+	} // end Update ()
+} // end Rotator
